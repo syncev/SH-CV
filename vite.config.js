9f19a406ddb1c4b1f688c9cb7d276e812b8f3b3a
Santiago Hernandez <santuche90@gmail.com>
2023-11-13 22:18:14 -0300

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "/SH-CV/"
+  base: "https://syncev.github.io/SH-CV/"
 })
