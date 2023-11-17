f8fc256336f62aff970ff81d148050ed37ae574c
Santiago Hernandez <santuche90@gmail.com>
2023-11-17 14:33:45 -0300

    about me style for mobile ready

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,18 +3,19 @@ import Me from "/src/assets/me.png";
 const AboutMe = () => {
   return (
     <div className="about-wrapper">
-      <h1 className="about-header">About Santiago Hernandez</h1>
-
-      <p>
-        Argentinian born in 1990. <br />
-        Speaks Spanish (native) and English fluently. <br />
-        Completed the University of Buenos Aires`s Front-End Developer Junior
-        course and Educado en Argentina`s Front-End Web Development in 2023
-        (both from Argentina Programa 4.0). <br />I view web development as a
+      <h1 className="about-header">About Me:</h1>
+      <p className="about-main">
+        Argentinian born in 1990 fluent in Spanish (native) and English. <br />
+        Completed the <strong>University of Buenos Aires</strong>`s <em>Front-End Developer Junior </em> 
+         course and <strong>Educado en Argentina</strong>`s <em>Front-End Web Development</em> in 2023
+        (both from Argentina Programa 4.0). <br />
+        </p>
+        <p className="about-main-cuote">
+        "I view web development as a
         dynamic canvas where creativity meets functionality, allowing me to
-        bring dreams to life in the digital realm.
+        bring dreams to life in the digital realm."
       </p>
-      <img src={Me} alt="my profile picture" className="profile-img" />
+      <img src={Me} alt="my profile picture" className="about-img" />
     </div>
   );
 };
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact-wrapper">
         Contact Me
-      <ul>
-        <li className="linkedin">
+      <ul className="contact-list">
+        <li className="contact-item linkedin">
           <a href="www.linkedin.com/in/san-hernandez">
             <img src="/src/assets/icons/linkedin.png" alt="" />
           </a>
         </li>
-        <li className="email">
+        <li className="contact-item email">
             <a href = "mailto: santiago.n.hernandez@gmail.com">
-                <img src="/src/assets/icons/manzana" alt="" />
+                <img src="/src/assets/icons/manzana.png" alt="" />
             </a>
             </li>
-        <li className="facebook">
+        <li className="contact-item facebook">
             <a href="https://www.facebook.com/santiago.n.hernandez/">
                 <img src="/src/assets/icons/facebook.png" alt="" />
             </a>
         </li>
-        <li className="instagram">
+        <li className="contact-item instagram">
             <img src="/src/assets/icons/instagram.png" alt="" />
             <a href="http://instagram.com/saintyago_hernandez">
             </a>
