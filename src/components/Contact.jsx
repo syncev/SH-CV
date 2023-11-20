bd0a4ae23eae57308c9b7b25e2739dcfaca73896
Santiago Hernandez <santuche90@gmail.com>
2023-11-20 19:03:16 -0300

    imported contact icons properly

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import linkedin from "/src/assets/icons/linkedin.png";
+import manzana from "/src/assets/icons/manzana.png"
+import facebook from "/src/assets/icons/facebook.png";
+import instagram from "/src/assets/icons/instagram.png";
 
 const Contact = () => {
   return (
@@ -9,21 +13,21 @@ const Contact = () => {
       <ul className="contact-list">
         <li className="contact-item linkedin">
           <a href="www.linkedin.com/in/san-hernandez">
-            <img src="/src/assets/icons/linkedin.png" alt="" />
+            <img src={linkedin} alt="" />
           </a>
         </li>
         <li className="contact-item email">
             <a href = "mailto: santiago.n.hernandez@gmail.com">
-                <img src="/src/assets/icons/manzana.png" alt="" />
+                <img src={manzana} alt="" />
             </a>
             </li>
         <li className="contact-item facebook">
             <a href="https://www.facebook.com/santiago.n.hernandez/">
-                <img src="/src/assets/icons/facebook.png" alt="" />
+                <img src={facebook} alt="" />
             </a>
         </li>
         <li className="contact-item instagram">
-            <img src="/src/assets/icons/instagram.png" alt="" />
+            <img src={instagram} alt="" />
             <a href="http://instagram.com/saintyago_hernandez">
             </a>
         </li>
