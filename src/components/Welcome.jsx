7fc724fbf78e1079fc65ac9df998d4d09780aa70
Santiago Hernandez <santuche90@gmail.com>
2023-11-21 14:33:34 -0300

    vh for all devices

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,7 +10,7 @@ const Welcome = () => {
     <div className='welcome-wrapper'>
         <section className={`welcome-section `}>
             <h1 className={`welcome-greetings ${pageLoaded ? "fadeIn" : ""}`}>Hi! Welcome to my Portfolio.</h1>
-            <p className={`welcome-description ${pageLoaded ? "fadeIn" : ""}`}>Below you will find all my projects</p>
+            <p className={`welcome-description ${pageLoaded ? "fadeIn" : ""}`}>Here you will find all my projects</p>
         </section>
     </div>
   )
