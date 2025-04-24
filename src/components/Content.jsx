e0a283995ce82b4f7a2f0c056e0c50e70d9313b1
Santiago Hernandez <santuche90@gmail.com>
2025-04-23 22:24:58 -0300

    fixed about me text

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,7 +4,7 @@ export const contentLanguage = {
             title: "Sobre Mi",
             p1a : "Hola, mi nombre es Santiago Hernández. He estado aprendiendo activamente desarrollo web desde 2021 y me gradué como ",
             p1b : "Desarrollador Frontend" ,
-            p1c: "del curso de Desarrollo Front End de Argentina Programa 4.0 en 2023. Desde entonces, he seguido ampliando mis conocimientos realizando distintos cursos online y creando proyectos personales para mejorar mis habilidades y explorar nuevas tecnologías.",
+            p1c: `de el curso de "Desarrollo Front End" de "Argentina Programa 4.0" en 2023, y desde entonces he continuado mi formación a través de programas acreditados mientras desarrollo proyectos personales para perfeccionar mis habilidades y explorar nuevas tecnologías.`,
 
             p2a : "Actualmente, mi objetivo es convertirme en ",
             p2b : "Desarrollador de Software",
@@ -43,7 +43,7 @@ export const contentLanguage = {
             title: "About Me",
             p1a : "Hi, my name is Santiago Hernandez, I've been actively learning web development since 2021 and graduated as ",
             p1b: "Frontend Developer",
-            p1c:"from the Argentina Programa 4.0 Front End Development course in 2023. Since then, I've been continuously expanding my knowledge by taking various online courses and building personal projects to sharpen my        skills and explore new technologies.",
+            p1c:"from the Argentina Programa 4.0 Front End Development course in 2023. Since then, I’ve continued my education through accredited programs while developing personal projects to sharpen my skills and explore new technologies.",
             p2a : "I´m currently aiming to become a ",
             p2b : "Software Developer",
             p2c : ", and I´m committed to growing both technically and professionally to reach that goal.",
